34e817469deab82e67a569a2099031ec48a573bb
Sun Junwen <sunjw8888@gmail.com>
2026-07-27 15:01:10 +0800

    fhash: Update fhash.rb

--- Casks/fhash.rb
+++ Casks/fhash.rb
@@ -1,29 +1,28 @@
 cask "fhash" do
   version "26.7.46"
-
-  # GitHub release universal DMG
+  sha256 "115051D5DC94B4DA1B0867F7AFC71274C966E65B2501D092A261E7ACF1CEFC20"
   url "https://github.com/sunjw/fhash/releases/download/#{version}/fHash-#{version}-macOS.dmg"
   name "fHash"
   desc "An open source files hash calculator for macOS."
   homepage "https://github.com/sunjw/fhash"
 
-  # fHash-26.7.46-macOS.dmg
-  sha256 "115051D5DC94B4DA1B0867F7AFC71274C966E65B2501D092A261E7ACF1CEFC20"
+  depends_on macos: ">= :sonoma"
 
   livecheck do
-    url :url
+    url "https://github.com/sunjw/fhash/releases"
     strategy :github_latest_release
   end
 
   app "fHash.app"
 
   zap trash: [
-    "~/Library/Containers/org.sunjw.fHashMacUI",
+    "~/Library/Containers/org.sunjw.fHashMacUI"
   ]
 
-  # fHash unsigned
   caveats <<~EOS
-    fHash is unsigned and not notarized. On the first launch, macOS Gatekeeper will block it.
-    To open: System Settings → Privacy & Security → scroll to the bottom → click "Open Anyway".
+    fHash is unsigned and not notarized. On the first run, the Gatekeeper will block the app.
+    To open it, go to System Settings → Privacy & Security, scroll to the bottom, and click Open Anyway for fHash.
+    Or, run xattr -dr com.apple.quarantine /Applications/fHash.app in Terminal to remove the Gatekeeper quarantine attribute.
+    Use these workarounds at your own risk.
   EOS
 end
